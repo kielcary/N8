ccb7927cba5730540ac5c1073313674ba8ee7d40
kielcary <kiel@thirddaydev.com>
2015-04-09 14:59:26 -0400

    Initial

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+function ShowProfile() {
+    document.getElementById("profile").style.display = 'block';
+    document.getElementById("insurance").style.display = 'none';
+}
+
+function ShowInsurance() {
+    document.getElementById("insurance").style.display = 'block';
+    document.getElementById("profile").style.display = 'none';
+}
